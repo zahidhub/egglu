cf5988d2bb9969125c430358ff3084307ce214c9
Hasan Zahid <guszahidha@student.gu.se>
2025-02-17 03:44:06 +0100

    header added on screen

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./Header";
 import "./Screen.css";
 
 function Screen(){
     return (
         <div className="screenSize">
-            
+            <Header/>
         </div>
     );
 }
