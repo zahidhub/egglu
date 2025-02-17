a0ccf51b6c63cea452fb3b9d7aab9087caf64e51
Hasan Zahid <guszahidha@student.gu.se>
2025-02-17 03:43:13 +0100

    header component implemeted

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header(){
+    return(
+        <header className = 'headerBackground headerSize'>
+            <div>
+                <img src='' alt='egglu logo' className='headerLogo' />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
