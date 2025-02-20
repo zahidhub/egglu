dfc04429eada232bfd71c05a2dcf30e1c7e10a79
Hasan Zahid <guszahidha@student.gu.se>
2025-02-20 06:46:49 +0100

    comments added for Body.jsx and Body.css

--- src/Body.jsx
+++ src/Body.jsx
@@ -4,15 +4,15 @@ import Card from "./Card"; // import the Card class
 
 function Body(){ 
     return(
-        <div className='bodyContainer bodyBackground'> 
-            <h4 className="bodyTextTypography">choose your egg type</h4>
-            <div className="bodyLayout">
-            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" />
-            <Card icon={'src/assets/softBoiled.png'} text="soft boiled" />
-            <Card icon={'src/assets/hardBoiled.png'} text="hard boiled" />
-            <Card icon={'src/assets/pritu.png'} text="pritus word" />
+        <div className='bodyContainer bodyBackground'> {/*initiating the css file for the body*/}
+            <h4 className="bodyTextTypography">choose your egg type</h4> {/*text body for the header*/}
+            <div className="bodyLayout"> {/*initiating the css file for the body to hold the cards*/}
+            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" /> {/*calling the function with props*/}
+            <Card icon={'src/assets/softBoiled.png'} text="soft boiled" /> {/*calling the function with props*/}
+            <Card icon={'src/assets/hardBoiled.png'} text="hard boiled" /> {/*calling the function with props*/}
+            <Card icon={'src/assets/pritu.png'} text="pritus word" /> {/*calling the function with props*/}
             </div>
         </div> 
     );
 }
-export default Body;
+export default Body; // exporting the body class
