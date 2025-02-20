dd576cbf147a0abad5781cfc294a17d104a103b7
Hasan Zahid <guszahidha@student.gu.se>
2025-02-20 02:25:11 +0100

    card.jsx implemented

--- src/Body.jsx
+++ src/Body.jsx
@@ -4,7 +4,10 @@ import Card from "./Card";
 function Body(){
     return(
         <div className='bodyLayout background'>
-            <Card icon={'src/assets/runnyYolk.png'} text="fire" />
+            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" />
+            <Card icon={'src/assets/softBoiled.png'} text="soft boiled" />
+            <Card icon={'src/assets/hardBoiled.png'} text="hard boiled" />
+            <Card icon={'src/assets/pritu.png'} text="pritus word" />
         </div>
     );
 }
