b8accebaf0f46323d88fd486fd7705de639d5947
Hasan Zahid <guszahidha@student.gu.se>
2025-02-21 04:37:44 +0100

    implemented onCick in Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,21 @@
-import "./Card.css";
+import "./Card.css"; // import the card style sheet
 
-function Card({icon, text}){
+function Card({icon, text, onClick}){
     return(
-        <button className="cardBackground cardLayout">
+        // decalring the style and onClick
+        <button className="cardBackground cardLayout" onClick={onClick}>
+            {/*store the icon for card */}
             <div className="icon">
                 <img src={icon} alt="icon" className="icon"/>
             </div>
+            {/*the title for card */}
             <div className="typography">
                 {text}
             </div>
+
         </button>
     );
 }
 
-export default Card;
+export default Card; // export the card
 
