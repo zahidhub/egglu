c399d2b0bc5584a7b26c8d2fae37303ddd1a8fdf
Hasan Zahid <guszahidha@student.gu.se>
2025-02-21 01:50:46 +0100

    Notification.jsx implemented for egglu

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -1,11 +1,16 @@
-import "./Notification.css";
+import "./Notification.css"; // import the style file
 
-function Notification(){
-    return(
+function Notification({ onClose }) { // passing a prop as argument
+    return (
         <div>
-
+            <div className="notiLayout notiBackground"> {/* decalring a div and passing the style values*/}
+                <p className="notiTypography"> {/*style for the text */}
+                    MADE TO HELP ‘PRITU’ <br /> she doesnt know how to boil an egg {/*text to show on the notification */}
+                </p>
+                <div className="xIconLayout" onClick={onClose}></div> {/*to perform the action to close, holds the 'X' icon */}
+            </div>
         </div>
     );
 }
 
-export default Notification;
+export default Notification; // export the notification
--- src/Screen.jsx
+++ src/Screen.jsx
@@ -1,16 +1,35 @@
-import Header from "./Header";
-import Body from "./Body";
-import BottomBar from "./BottomBar";
-import "./Screen.css";
+import { useState } from "react"; // import state from react
+import Header from "./Header"; // import header
+import Body from "./Body"; // import body
+import BottomBar from "./BottomBar"; // import the bottomBar
+import Notification from "./Notification"; // import notification
+import "./Screen.css"; // import screen style sheet
 
-function Screen(img, ){
+function Screen() { 
+    const [showNotification, setShowNotification] = useState(true); {/*the notification is show in the beginning and that updted in setShowNotification*/}
+
+    const handleCloseNotification = () => {
+        setShowNotification(false); // when user clicks X, this turns showNotification false
+    };
+
     return (
-        <div className="screenSize">
-            <Header/>
-            <Body/>
-            <BottomBar/>
+        <div className="screenSize"> {/*apply style for the screen */}
+
+            {/*show notification component till true, closed once user clicks X */}
+            {showNotification && (
+                <Notification onClose={handleCloseNotification} /> 
+            )}
+
+            <Header />
+
+            {/*the className, component depends on the showNotification, if true it will have an extra class blurred, else bodyContainer applied only */}
+            <div className={`bodyContainer ${showNotification ? "blurred" : ""}`}> {/*bluring only the body*/}
+                <Body />
+            </div>
+
+            <BottomBar /> {/*bottombar component */}
         </div>
     );
 }
 
-export default Screen;
+export default Screen; //export the screen
