9668fd17f654a0a6e89a07f61a9d918933ff0a16
Hasan Zahid <guszahidha@student.gu.se>
2025-02-21 06:09:11 +0100

    updated logo path in index.html

--- src/Body.jsx
+++ src/Body.jsx
@@ -7,7 +7,7 @@ function Body({onCardClick}){
         <div className='bodyContainer bodyBackground'> {/*initiating the css file for the body*/}
             <h4 className="bodyTextTypography">choose your egg type</h4> {/*text body for the header*/}
             <div className="bodyLayout"> {/*initiating the css file for the body to hold the cards*/}
-            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" onClick={() => onCardClick(1)} /> {/*calling the function with props*/}
+            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" onClick={() => onCardClick(0.5)} /> {/*calling the function with props*/}
             <Card icon={'src/assets/softBoiled.png'} text="soft boiled" onClick={() => onCardClick(2)} /> {/*calling the function with props*/}
             <Card icon={'src/assets/hardBoiled.png'} text="hard boiled" onClick={() => onCardClick(3)} /> {/*calling the function with props*/}
             <Card icon={'src/assets/pritu.png'} text="pritus word" /> {/*calling the function with props*/}
