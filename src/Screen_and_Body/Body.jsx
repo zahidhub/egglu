8ebbbd70d444cfe49c223a1b33ac6f17d6496bf5
Hasan Zahid <guszahidha@student.gu.se>
2025-02-22 18:18:28 +0100

    boiling time added in Body.jsx

--- src/Screen_and_Body/Body.jsx
+++ src/Screen_and_Body/Body.jsx
@@ -7,9 +7,9 @@ function Body({onCardClick}){
         <div className='bodyContainer bodyBackground'> {/*initiating the css file for the body*/}
             <h4 className="bodyTextTypography">choose your egg type</h4> {/*text body for the header*/}
             <div className="bodyLayout"> {/*initiating the css file for the body to hold the cards*/}
-            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" onClick={() => onCardClick(0.1)} /> {/*calling the function with props*/}
-            <Card icon={'src/assets/softBoiled.png'} text="soft boiled" onClick={() => onCardClick(2)} /> {/*calling the function with props*/}
-            <Card icon={'src/assets/hardBoiled.png'} text="hard boiled" onClick={() => onCardClick(3)} /> {/*calling the function with props*/}
+            <Card icon={'src/assets/runnyYolk.png'} text="runny yolk" onClick={() => onCardClick(6)} /> {/*calling the function with props*/}
+            <Card icon={'src/assets/softBoiled.png'} text="soft boiled" onClick={() => onCardClick(8)} /> {/*calling the function with props*/}
+            <Card icon={'src/assets/hardBoiled.png'} text="hard boiled" onClick={() => onCardClick(10)} /> {/*calling the function with props*/}
             <Card icon={'src/assets/pritu.png'} text="pritus word" /> {/*calling the function with props*/}
             </div>
         </div> 
