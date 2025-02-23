d92d5ed30a82e289b4b4712abc632986ec9392c1
Hasan Zahid <guszahidha@student.gu.se>
2025-02-23 04:39:30 +0100

    upadted intro message in Notification.jsx

--- src/PopUps/Notification.jsx
+++ src/PopUps/Notification.jsx
@@ -5,7 +5,7 @@ function Notification({ onClose }) { // passing a prop as argument
         <div>
             <div className="notiLayout notiBackground"> {/* decalring a div and passing the style values*/}
                 <p className="notiTypography"> {/*style for the text */}
-                    MADE TO HELP ‘PRITU’ <br /> she doesnt know how to boil an egg {/*text to show on the notification */}
+                    LET ME HELP YOU BOIL AN EGG<br />I AM CHICKEN {/*text to show on the notification */}
                 </p>
                 <div className="xIconLayout" onClick={onClose}></div> {/*to perform the action to close, holds the 'X' icon */}
             </div>
