fdd7aa65478dcf3e1fd40f1776ac1358cd5786ca
Hasan Zahid <guszahidha@student.gu.se>
2025-02-23 04:50:29 +0100

    comment for Header.jsx and .css git

--- src/Header_and_Footer/Header.jsx
+++ src/Header_and_Footer/Header.jsx
@@ -3,9 +3,11 @@ import "./Header.css";
 function Header(){
     return(
         <header className = 'headerBackground headerSize'>
+
             <div>
-                <img src='src/assets/eggluLogo.png' alt='egglu logo' className='headerLogo' />
+                <img src='src/assets/eggluLogo.png' alt='egglu logo' className='headerLogo' /> {/*holds the logo*/} 
             </div>
+
         </header>
     );
 };
