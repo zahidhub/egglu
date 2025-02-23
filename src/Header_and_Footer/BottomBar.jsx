494ac790db146c1ac054d9690770a81740443121
Hasan Zahid <guszahidha@student.gu.se>
2025-02-23 04:47:20 +0100

    comments for BottomBar.jsx and .css

--- src/Header_and_Footer/BottomBar.jsx
+++ src/Header_and_Footer/BottomBar.jsx
@@ -3,9 +3,11 @@ import "./BottomBar.css";
 function BottomBar(){
     return(
         <div className="footerLayout">
+
             <div className="footerTypography footerTextAign">
-                <h4>made by hasan zahid</h4>
+                <h4>made by hasan zahid</h4> {/*footer text*/}
             </div>
+
         </div>
     );
 }
